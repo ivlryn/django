76986e84816c1ac5ff111a6f3d06ecbb78c9d36d
ivlryn <Neththeng264@gmail.com>
2025-12-20 07:59:20 +0700

    Fix ALLOWED_HOSTS for Render deployment

--- aura/settings.py
+++ aura/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wq%*l#z82&l0mkgmd1*dz8fbbaiy@ggg*cuuq-(wuefdp^hom6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
